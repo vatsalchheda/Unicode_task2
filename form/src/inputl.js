1f848810c208cfa7ba27a2fb56709475b1007c53
vatsalchheda <vatsalchheda007@gmail.com>
2019-12-03 00:28:01 +0530

    [First Commit]

---
+++ form/src/inputl.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class inputl extends React.Component{
+    render(){
+        return <h2>Hello World</h2>
+    }
+}
+
+export default inputl;
